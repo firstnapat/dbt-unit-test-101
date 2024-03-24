4c5c74f67b9997e66ca8cca6ee1c8d9f7f1fcd0e
Firstnapat <firstnapat@odds.team>
2024-03-24 18:33:44 +0700

    test: add unit test for test when order_id was not matched it should return no rows

---
+++ tests/unit/staging/stg_convert_thai_baht_to_us_dollars_unit_test/test_when_order_id_was_not_matched_it_should_return_no_rows.sql
@@ -0,0 +1,19 @@
+-- depends_on: {{ ref('stg_payments_raw') }}
+-- depends_on: {{ ref('stg_customers_raw') }}
+{{ config(tags=['unit-test']) }}
+
+{% call dbt_unit_testing.test('stg_convert_thai_baht_to_us_dollars', 
+'test when order_id was not matched it should return no rows') %}
+  {% call dbt_unit_testing.mock_ref ('stg_payments_raw', {"input_format": "csv"}) %}
+    id,order_id,payment_method,thai_amount
+    1,1,'credit_card',100
+  {% endcall %}
+  
+  {% call dbt_unit_testing.mock_ref ('stg_customers_raw', {"input_format": "csv"}) %}
+    id,first_name,last_name
+    45,'Thomas','B.'
+    {% endcall %}
+
+  {% call dbt_unit_testing.expect_no_rows() %}
+  {% endcall %}
+{% endcall %}
