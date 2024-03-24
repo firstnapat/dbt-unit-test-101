c9b8a5c17b9c995ff0cf844406cc6796b277cf29
Firstnapat <firstnapat@odds.team>
2024-03-24 18:34:33 +0700

    test: add unit test for test when order_id was not matched it should return no rows pipe format

--- tests/unit/staging/stg_convert_thai_baht_to_us_dollars_unit_test/test_when_order_id_was_not_matched_it_should_return_no_rows_pipe_format.sql
+++ tests/unit/staging/stg_convert_thai_baht_to_us_dollars_unit_test/test_when_order_id_was_not_matched_it_should_return_no_rows_pipe_format.sql
@@ -3,7 +3,7 @@
 {{ config(tags=['unit-test']) }}
 
 {% call dbt_unit_testing.test('stg_convert_thai_baht_to_us_dollars', 
-'test when order_id was not matched it should return no rows') %}
+'test when order_id was not matched it should return no rows pipe format') %}
   {% call dbt_unit_testing.mock_ref ('stg_payments_raw', {"input_format": "csv", "column_separator": "|"}) %}
     id  | order_id | payment_method | thai_amount::int
     1   | 1        | 'credit card'  | 50000
